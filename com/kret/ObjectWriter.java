4e89ed835c433b60bdeac8ccc34c968b96054356
Yuri <kret9bfg@gmail.com>
2021-10-26 13:19:13 +0300

    aded ObjectWriter

--- com/kret/ObjectWriter.java
+++ com/kret/ObjectWriter.java
@@ -5,12 +5,11 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class ObjectWriter {
-    public static void writeObject( Object a) {
+     protected void writeObject( Order a) {
         try {
             FileOutputStream fos = new FileOutputStream("order.txt");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
             oos.writeObject(a);
-
             oos.close();
         } catch (IOException e) {
             e.printStackTrace();
